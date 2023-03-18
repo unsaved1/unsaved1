9adb0dde6b3d11f47e7354334d31e207592fa9d1
wrld <eelwrld@gmail.com>
2023-03-18 22:43:01 +0500

    post request to php server

---
+++ dist/js/promise.js
@@ -0,0 +1,55 @@
+'use strict';
+
+// const num = 2;
+
+// console.log('запрос данных...')
+
+// const req = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('подготовка данных...');
+
+//         const product = {
+//             name: 'TV',
+//             price: 2000
+//         };
+
+//         resolve(product);
+
+//     }, 2000)
+// }); 
+
+// req.then(data => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             data.status = 'order';
+//             resolve(data);
+//         }, 2000)
+//     }) 
+// }).then(data => {
+//     data.price = 3222;
+//     console.log('sync')
+//     return (data);
+// }).then(data => {
+//     console.log(data);
+// }).catch(() => {
+//     console.log('error');
+// }).finally(() => {
+//     console.log('finally')
+// })
+
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time)
+    })
+};
+
+// test(1000).then(() => console.log('1000 ms'));
+// test(2000).then(() => console.log('2000 ms'));
+
+
+Promise.all([test(1000), test(2000)]).then(() => {
+    console.log('all');
+})
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('all');
+})
